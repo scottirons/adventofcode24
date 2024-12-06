6c2c93480ee7e56f59a93f3229d1dad5b556d7a0
scottpirons@gmail.com <87613441+scottirons@users.noreply.github.com>
2024-12-06 16:33:11 -0700

    part b done. tried doing cycle checking along the way but it was janky, so I just decided to check all possibilities after I was done

--- day6/solve.py
+++ day6/solve.py
@@ -1,25 +1,23 @@
 class Directions:
     def __init__(self, start):
         self.dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        paths = {
-            ">": 0,
-            "v": 1,
-            "<": 2,
-            "^": 3
-        }
-        self.start = paths[start]
+        self.start = start
+        self.curr = self.start
+    
+    def get_start(self):
+        return self.start
     
     def next(self):
-        self.start = (self.start + 1) % 4
+        self.curr = (self.curr + 1) % 4
     
     def peek_next(self):
-        return self.dirs[(self.start + 1) % 4]
+        return self.dirs[(self.curr + 1) % 4]
     
     def get_curr(self):
-        return self.dirs[self.start]
+        return self.dirs[self.curr]
     
     def get_opp(self):
-        return self.dirs[(self.start + 2) % 4]
+        return self.dirs[(self.curr + 2) % 4]
 
 class Solution:
 
@@ -29,30 +27,38 @@ class Solution:
         self.part_a = 1
         self.part_b = 0
         self.visited = set()
-        self.dir_vis = set()
         self.curr, self.dir = self.find_start()
-        self.dir_vis.add((self.curr, self.dir.get_curr()))
-        self.extend_tail()
-    
+        
     def find_start(self):
+        paths = {
+            ">": 0,
+            "v": 1,
+            "<": 2,
+            "^": 3
+        }
         for i in range(len(self.data)):
             for j in range(len(self.data[0])):
                 if self.data[i][j] not in "#.":
-                    dirs = Directions(self.data[i][j])
+                    dirs = Directions(paths[self.data[i][j]])
                     self.visited.add((i, j))
                     return (i, j), dirs
     
-    def extend_tail(self):
-        curr_dir = self.dir.get_curr()
-        opp = self.dir.get_opp()
-        a, b = self.curr[0], self.curr[1]
-        while a >= 0 and a < len(self.data) and b >= 0 and b < len(self.data[0]):
-            if self.data[a][b] == "#":
+    def check_cycle(self, start, dir, new_block):
+        visited = {(start, dir.get_curr())}
+        curr = start
+        while True:
+            a, b = curr[0] + dir.get_curr()[0], curr[1] + dir.get_curr()[1]
+            if a < 0 or a >= len(self.data) or b < 0 or b >= len(self.data[0]):
                 break
-            self.dir_vis.add(((a, b), curr_dir))
-            a += opp[0]
-            b += opp[1]
-
+            nc = self.data[a][b] if (a, b) != new_block else "#"
+            if nc == "#":
+                dir.next()
+            else:
+                if ((a, b), dir.get_curr()) in visited:
+                    return True
+                visited.add(((a, b), dir.get_curr()))
+                curr = (a, b)
+        return False
 
     def solve(self):
         while True:
@@ -62,17 +68,18 @@ class Solution:
             nc = self.data[a][b]
             if nc == "#":
                 self.dir.next()
-                self.extend_tail()
             else:
                 if (a, b) not in self.visited:
                     self.part_a += 1
                 self.visited.add((a, b))
-                if (self.curr, self.dir.peek_next()) in self.dir_vis:
-                    self.part_b += 1
-                self.dir_vis.add((self.curr, self.dir.get_curr()))
                 self.curr = (a, b)
+        # now part B
+        start, dir = self.find_start()
+        self.visited.remove(start)
+        for loc in self.visited:
+            self.part_b += self.check_cycle(start, Directions(dir.get_start()), loc)
         return self.part_a, self.part_b
-        
+
 
 sol = Solution("input.txt")
 a, b = sol.solve()
