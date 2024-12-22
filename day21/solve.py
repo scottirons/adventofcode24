4bd6667de464ba38f5fe12bf4db5ea6afd1c34e0
scottpirons@gmail.com <87613441+scottirons@users.noreply.github.com>
2024-12-21 19:42:36 -0700

    day 21 done. Did not figure out all of this on my own, and I was too burnt to try to programatically figure out the best paths between any two nodes, so I opted to just hard code it all with what I assumed would be the best path (keeping similar button presses contiguous. For example, '>>v' is preferable to '>v>', even though they're the same length, because the previous robot won't have to go back and forth an extra time).

--- day21/solve.py
+++ day21/solve.py
@@ -1,6 +1,8 @@
 from functools import cache
+from itertools import pairwise
 from math import inf
 import re
+from time import perf_counter
 
 KEYS = {
     '>':
@@ -15,100 +17,203 @@ KEYS = {
         {'>': None, 'v': '>', '<': '^', '^': None},
 }
 
+NUMS = {
+    '0':
+        {'>': 'A', 'v': None, '<': None, '^': '2'},
+    '1':
+        {'>': '2', 'v': None, '<': None, '^': '4'},
+    '2':
+        {'>': '3', 'v': '0', '<': '1', '^': '5'},
+    '3':
+        {'>': None, 'v': 'A', '<': '2', '^': '6'},
+    '4':
+        {'>': '5', 'v': '1', '<': None, '^': '7'},
+    '5':
+        {'>': '6', 'v': '2', '<': '4', '^': '8'},
+    '6':
+        {'>': None, 'v': '3', '<': '5', '^': '9'},
+    '7':
+        {'>': '8', 'v': '4', '<': None, '^': None},
+    '8':
+        {'>': '9', 'v': '5', '<': '7', '^': None},
+    '9':
+        {'>': None, 'v': '6', '<': '8', '^': None},
+    'A':
+        {'>': None, 'v': None, '<': '0', '^': '3'},
+}
 
-def move_dir(curr, button):
-    return KEYS[curr][button]
+KEY_BEST = {
+    '>': {
+        'v': '<A',
+        '<': '<<A',
+        '^': '<^A',
+        'A': '^A',
+        '>': 'A'
+    },
+    '<': {
+        'v': '>A',
+        '>': '>>A',
+        '^': '>^A',
+        'A': '>>^A',
+        '<': 'A'
+    },
+    'v': {
+        '>': '>A',
+        '<': '<A',
+        '^': '^A',
+        'A': '^>A',
+        'v': 'A'
+    },
+    '^': {
+        'v': 'vA',
+        '<': 'v<A',
+        '>': 'v>A',
+        'A': '>A',
+        '^': 'A'
+    },
+    'A': {
+        'v': '<vA',
+        '<': 'v<<A',
+        '^': '<A',
+        '>': 'vA',
+        'A': 'A'
+    }
+}
 
+NUM_BEST = {
+    '0': {'1': '^<A', '2': '^A', '3': '^>A', '4': '^^<A', '5': '^^A', '6': '^^>A', '7': '^^^<A', '8': '^^^A', '9': '^^^>A', 'A': '>A', '0': 'A'},
+    '1': {'0': '>vA', '2': '>A', '3': '>>A', '4': '^A', '5': '^>A', '6': '^>>A', '7': '^^A', '8': '^^>A', '9': '^^>>A', 'A': '>>vA', '1': 'A'},
+    '2': {'1': '<A', '0': 'vA', '3': '>A', '4': '<^A', '5': '^A', '6': '^>A', '7': '<^^A', '8': '^^A', '9': '^^>A', 'A': 'v>A', '2': 'A'},
+    '3': {'1': '<<A', '2': '<A', '0': '<vA', '4': '<<^A', '5': '<^A', '6': '^A', '7': '<<^^A', '8': '<^^A', '9': '^^A', 'A': 'vA', '3': 'A'},
+    '4': {'1': 'vA', '2': 'v>A', '3': 'v>>A', '0': '>vvA', '5': '>A', '6': '>>A', '7': '^A', '8': '^>A', '9': '^>>A', 'A': '>>vvA', '4': 'A'},
+    '5': {'1': '<vA', '2': 'vA', '3': 'v>A', '4': '<A', '0': 'vvA', '6': '>A', '7': '<^A', '8': '^A', '9': '^>A', 'A': 'vv>A', '5': 'A'},
+    '6': {'1': '<<vA', '2': '<vA', '3': 'vA', '4': '<<A', '5': '<A', '0': '<vvA', '7': '<<^A', '8': '<^A', '^9': 'A', 'A': 'vvA', '6': 'A'},
+    '7': {'1': 'vvA', '2': 'vv>A', '3': 'vv>>A', '4': 'vA', '5': 'v>A', '6': 'v>>A', '0': '>vvvA', '8': '>A', '9': '>>A', 'A': '>>vvvA', '7': 'A'},
+    '8': {'1': '<vvA', '2': 'vvA', '3': 'vv>A', '4': '<vA', '5': 'vA', '6': 'v>A', '7': '<A', '0': 'vvvA', '9': '>A', 'A': 'vvv>A', '8': 'A'},
+    '9': {'1': '<<vvA', '2': '<vvA', '3': 'vvA', '4': '<<vA', '5': '<vA', '6': 'vA', '7': '<<A', '8': '<A', '0': '<vvvA', 'A': 'vvvA', '9': 'A'},
+    'A': {'1': '^<<A', '2': '<^A', '3': '^A', '4': '^^<<A', '5': '<^^A', '6': '^^A', '7': '^^^<<A', '8': '<^^^A', '9': '^^^A', '0': '<A', 'A': 'A'},
+}
 
-def move_number(curr, button):
-    if curr == 'A':
-        return '0' if button == '<' else '3' if button == '^' else None
-    elif curr == '0':
-        return 'A' if button == '>' else '2' if button == '^' else None
-    elif curr == '2' and button == 'v':
-        return '0'
-    elif curr == '3' and button == 'v':
-        return 'A'
-    curr_int = int(curr)
-    if button == '^':
-        result = curr_int + 3
-        if result not in {7, 8, 9, 4, 5, 6}:
-            return None
-        return str(result)
-    if button == '>':
-        result = curr_int + 1
-        if result not in {8, 5, 2, 9, 6, 3}:
-            return None
-        return str(result)
-    if button == '<':
-        result = curr_int - 1
-        if result not in {7, 4, 1, 8, 5, 2}:
-            return None
-        return str(result)
-    if button == 'v':
-        result = curr_int - 3
-        if result not in {4, 5, 6, 1, 2, 3}:
-            return None
-        return str(result)
-
-
-def cascade(r1, r2, keypad):
-    next_val = None
-    if r1 == 'A' and r2 == 'A':
-        next_val = keypad
-    elif r1 == 'A':
-        keypad = move_number(keypad, r2)
-    else:
-        r2 = move_dir(r2, r1)
-    return r1, r2, keypad, next_val
+# def move_dir(curr, button):
+#     return KEYS[curr][button]
+#
+#
+# def move_number(curr, button):
+#     return NUMS[curr][button]
+#
+# def find_best_path(start, end):
+#     path = ''
+#     return path
+#
+#
+#
+#
+# def cascade(locations):
+#     locs_list = list(locations)
+#     next_val = None
+#     valid = True
+#     i = 0
+#     while i < len(locs_list) - 1:
+#         r1 = locs_list[i]
+#         r2 = locs_list[i + 1]
+#         # just move the next one in the sequence and break
+#         if r1 in '<^v>':
+#             if i == len(locs_list) - 2:
+#                 r2 = move_number(r2, r1)
+#             else:
+#                 r2 = move_dir(r2, r1)
+#             if not r2:
+#                 valid = False
+#             locs_list[i + 1] = r2
+#             break
+#         #
+#         elif r1 == 'A':
+#             if i == len(locs_list) - 2:
+#                 next_val = r2
+#                 break # don't need to but oh well
+#             i += 1
+#
+#     return tuple(locs_list), valid, next_val
 
 
 class Solution:
-    def __init__(self, input):
-        with open(input, 'r') as f:
+    def __init__(self, source):
+        with open(source, 'r') as f:
             self.targets = f.read().splitlines()
         self.memo = {}
 
-    def get_paths(self, r1, r2, keypad, presses, target, seq):
-        if presses > 100:
-            return inf
-        if (r1, r2, keypad, target) in self.memo and self.memo[(r1, r2, keypad, target)] <= presses:
-            return inf
-        self.memo[(r1, r2, keypad, target)] = presses
-        if target == '':
-            return presses
-        result = inf
+    # def get_paths(self, locs, presses, target):
+    #     if (locs, target) in self.memo and self.memo[(locs, target)] <= presses:
+    #         return inf
+    #     self.memo[(locs, target)] = presses
+    #     if target == '':
+    #         return presses
+    #     result = inf
+    #
+    #     # press A
+    #     next_locs, valid, button_press = cascade(locs)
+    #     if valid:
+    #         if button_press is None:
+    #             result = min(result, self.get_paths(next_locs, presses + 1, target))
+    #         elif button_press == target[0]:
+    #             result = min(result, self.get_paths(next_locs, presses + 1, target[1:]))
+    #
+    #     loc_list = list(locs)
+    #     for button in 'v<^>':
+    #         r1_next = move_dir(locs[0], button)
+    #         if r1_next:
+    #             loc_list[0] = r1_next
+    #             result = min(self.get_paths(tuple(loc_list), presses + 1, target), result)
+    #
+    #     return result
+    #
+    # def solve(self, part_b=False):
+    #     total = 0
+    #
+    #     for target in self.targets:
+    #         self.memo = {}
+    #         locations = ('A', 'A', 'A')
+    #         if part_b:
+    #             locations = tuple('A' for _ in range(4))
+    #         path_cost = self.get_paths(locations, 0, target)
+    #         print(f"Target: {target}, Path Cost: {path_cost}")
+    #         int_part = re.match(r'(\d+)', target)
+    #         total += int(int_part[0]) * path_cost
+    #
+    #     return total
 
-        # press A
-        r1_next, r2_next, keypad_next, button_press = cascade(r1, r2, keypad)
-        if r1_next is None or r2_next is None or keypad_next is None:
-            pass
-        elif button_press is None:
-            result = min(self.get_paths(r1_next, r2_next, keypad_next, presses + 1, target, seq + 'A'), result)
-        elif button_press != target[0]:
-            pass
-        elif button_press == target[0]:
-            result = min(self.get_paths(r1_next, r2_next, keypad_next, presses + 1, target[1:], seq + 'A'), result)
+    @cache
+    def search(self, seq, level, nums=True):
+        res = 0
+        seq = "A" + seq
+        for u, v in pairwise(seq):
+            if nums:
+                new_path = NUM_BEST[u][v]
+            else:
+                new_path = KEY_BEST[u][v]
+            if level == 0:
+                res += len(new_path)
+            else:
+                res += self.search(new_path, level - 1, False)
+        return res
 
-        for button in 'v<^>':
-            r1_next = move_dir(r1, button)
-            if r1_next:
-                result = min(self.get_paths(r1_next, r2, keypad, presses + 1, target, seq + button), result)
-
-        return result
-
-    def solve(self):
-        total = 0
-
+    def solve_2(self):
+        result1 = 0
+        result2 = 0
         for target in self.targets:
-            self.memo = {}
-            path_cost = self.get_paths('A', 'A', 'A', 0, target, '')
-            print(f"Target: {target}, Path Cost: {path_cost}")
-            int_part = re.match(r'(\d+)', target)
-            total += int(int_part[0]) * path_cost
+            val1 = self.search(target, 2)
+            val2 = self.search(target, 25)
+            # print(f'Target: {target}, Path Cost: {val1}')
+            result1 += int(target[:-1]) * val1
+            result2 += int(target[:-1]) * val2
+        return result1, result2
 
-        return total
 
+
+start = perf_counter()
 sol = Solution('input.txt')
-a = sol.solve()
-print(f"Answer: {a}")
+a, b = sol.solve_2()
+print(f"A: {a}")
+print(f"B: {b}")
+print(f"Time: {perf_counter() - start}")
+
